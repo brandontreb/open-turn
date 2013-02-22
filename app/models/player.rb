dea251e08cc1643d02eac94283270056ed3f66ad
Brandon Trebitowski <brandontreb@gmail.com>
2013-02-22 11:06:01 -0700

    login

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,11 +1,14 @@
 class Player < ActiveRecord::Base
 
+  validates :username, :uniqueness => true
+
   default_scope includes(:games_players).order('games_players.turn_order ASC')
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :token_authenticatable
-
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :username
+  
+  attr_accessor :login
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :login
   has_many :games_players
   has_many :games, :through => :games_players
   has_many :turns
@@ -20,5 +23,13 @@ class Player < ActiveRecord::Base
   has_many :sent_invitations, :through => :invitations, :source => :player
   has_many :received_invitations, :class_name => "Invitation", :foreign_key => "invited_player_id"
 
+  def self.find_first_by_auth_conditions(warden_conditions)
+      conditions = warden_conditions.dup
+      if login = conditions.delete(:login)
+        where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+      else
+        where(conditions).first
+      end
+    end
 
 end
