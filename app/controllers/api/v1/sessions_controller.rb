1613e2cdb6f54e3574850ce30378b761125022b2
Brandon Trebitowski <brandontreb@gmail.com>
2013-02-23 07:36:48 -0700

    updated login

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,40 +2,40 @@ class Api::V1::SessionsController < Devise::SessionsController
 
   respond_to :json
 
-  # def create
-  #   email = params[:email]
-  #   password = params[:password]
-  #   if request.format != :json
-  #     render :status=>406, :json=>{:message=>"The request must be json"}
-  #     return
-  #   end
-
-  #   if login.nil? or password.nil?
-  #      render :status=>400,
-  #             :json=>{:message=>"The request must contain an email/username and password."}
-  #      return
-  #   end
-
-  #   @player=Player.find_by_email(email.downcase)
-
-  #   if @player.nil?      
-  #     render :status=>401, :json=>{:message=>"Invalid email or password."}
-  #     return
-  #   end
-
-  #   # http://rdoc.info/github/plataformatec/devise/master/Devise/Models/TokenAuthenticatable
-  #   @player.ensure_authentication_token!
-
-  #   if not @player.valid_password?(password)      
-  #     render :status=>401, :json=>{:message=>"Invalid email or password."}
-  #   else
-  #     render :status=>200, :json=>{:token=>@player.authentication_token}
-  #   end
-  # end
   def create
-    logger.info "Attempt to sign in by #{ params }"
-    super
+    email = params[:email]
+    password = params[:password]
+    if request.format != :json
+      render :status=>406, :json=>{:message=>"The request must be json"}
+      return
+    end
+
+    if login.nil? or password.nil?
+       render :status=>400,
+              :json=>{:message=>"The request must contain an email/username and password."}
+       return
+    end
+
+    @player=Player.find(:first, :conditions => [ "lower(username) = ? or lower(email) = ?", login.downcase, login.downcase])
+
+    if @player.nil?      
+      render :status=>401, :json=>{:message=>"Invalid email or password."}
+      return
+    end
+
+    # http://rdoc.info/github/plataformatec/devise/master/Devise/Models/TokenAuthenticatable
+    @player.ensure_authentication_token!
+
+    if not @player.valid_password?(password)      
+      render :status=>401, :json=>{:message=>"Invalid email or password."}
+    else
+      render :status=>200, :json=>{:token=>@player.authentication_token}
+    end
   end
+  # def create
+  #   logger.info "Attempt to sign in by #{ params }"
+  #   super
+  # end
 
   def destroy
     super
